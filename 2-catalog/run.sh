1f6047cf43d2c6a35a348b198459eb72450a71ef
mfrancia <m.francia@unibo.it>
2025-10-21 15:23:49 +0200

    fix nuke

--- 2-catalog/run.sh
+++ 2-catalog/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 datahub docker quickstart --stop | true
-datahub docker quickstart nuke | true
+datahub docker nuke | true
 datahub docker quickstart
 # datahub docker ingest-sample-data
 datahub ingest -c ingest-config.yml
